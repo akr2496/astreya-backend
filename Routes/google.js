5e43417a1a81fd7ffbca00f15c4bf58fd35b4bb1
akr2496 <akr2496@gmail.com>
2024-04-12 19:55:22 +0530

    google api for query run, list table, list database

--- Routes/google.js
+++ Routes/google.js
@@ -3,17 +3,23 @@ const express = require('express');
 const { BigQuery } = require('@google-cloud/bigquery');
 
 const router = express.Router();
-const keyFilename = '/Users/ashishranjan/Desktop/Personal/InterviewPrep/astreya/backend/astreya-testing.json'
+const keyFilename = '/Users/ashishranjan/Desktop/Personal/InterviewPrep/astreya/backend/astreya-testing1.json'
 const bigquery = new BigQuery({keyFilename});
+// const bigquery = new BigQuery();
 
+/*************************************************************************************************************
+ * 
+ * Database logics
+ *************************************************************************************************************/
 
 async function listDatasets() {
   try {
     // List datasets in the public project
-    const [datasets] = await bigquery.getDatasets({ projectId: 'bigquery-public-data' });
+    // const [datasets] = await bigquery.getDatasets({ projectId: 'bigquery-public-data' });
+    const [datasets] = await bigquery.getDatasets({ projectId: 'astreya-testing' });
     let data = [];
     datasets.forEach(dataset => {
-      const dataItem = {name : dataset.id, expanded: false};
+      const dataItem = {name : dataset.id, tables: [], lastFetchTime : null, expanded: false};
       data.push(dataItem);
     });
     return data;
@@ -22,7 +28,39 @@ async function listDatasets() {
     console.error('Error listing datasets:', error);
   }
 }
-// Endpoint for executing a BigQuery query
+
+async function listTables(datasetId) {
+  try {
+    // List tables in the specified dataset
+    
+    const [data] = await bigquery.dataset(datasetId).getTables();
+    
+    let tables = [];
+    data.forEach(table => {
+      tables.push(table.id)
+    });
+    return tables;
+  } catch (error) {
+    console.error('Error listing tables:', error);
+  }
+}
+
+async function runQuery(sqlQuery) {
+  try {
+    // Run the SQL query
+    const [result] = await bigquery.query(sqlQuery);
+    console.log(result)
+    return [result]
+  } catch (error) {
+    console.error('Error running query:', error);
+  }
+}
+
+/*************************************************************************************************************
+ * 
+ * Endpoint for executing a BigQuery query
+ *************************************************************************************************************/
+
 router.get('/', async (req, res) => {
   console.log('indide google routes');
   // console.log(bigquery);
@@ -32,13 +70,37 @@ router.get('/', async (req, res) => {
 })
 
 
-router.get('/google/query', async (req, res) => {
+router.get('/query', async (req, res) => {
   // const query = req.query.query;
   const query = `
 
   `
   try {
     const [rows] = await bigquery.query(query);
+    res.json(rows);
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'An error occurred while executing the query.' });
+  }
+});
+
+router.get('/getTables/:datasetId', async (req, res) => {
+  
+  const datasetId = req.params.datasetId;
+  console.log(`getting table for ${datasetId}`);
+  const tables = await listTables(datasetId);
+  console.log(tables);
+  res.json(tables);
+});
+
+router.post('/run', async (req, res) => {
+  const { sqlQuery } = req.body;
+  
+  if (!sqlQuery) {
+    return res.status(400).json({ error: 'SQL query is required.' });
+  }
+  try {
+    const [rows] = await runQuery(sqlQuery);
     res.json(rows);
   } catch (error) {
     console.error('Error executing query:', error);
--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express') ;
 const googleRoutes = require('./Routes/google');
 const snowflakeRoutes = require('./Routes/snowflake');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 8080;
 
 app.use(cors());
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
     console.log('Hi');
     res.json('Welcome to Backend')
